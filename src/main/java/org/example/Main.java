02a4a8f0a804a02413eb9c86b88e69e02994944c
Sébastien Deleuze <141109+sdeleuze@users.noreply.github.com>
2025-03-10 15:14:35 +0100

    Generic nullness demo (not catched by NullAway)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+
 public class Main {
+
 	public static void main(String[] args) {
-		TokenExtractor extractor = new DefaultTokenExtractor();
-		String token = extractor.extractToken("...");
-		System.out.println("The token has a length of " + token.length());
+		List<@Nullable String> listOfNullable = new ArrayList<>();
+		listOfNullable.add("foo");
+		listOfNullable.add(null);
+		listOfNullable.add("bar");
+		for (String element : listOfNullable) {
+			System.out.println(element.toLowerCase());
+		}
 	}
 }
