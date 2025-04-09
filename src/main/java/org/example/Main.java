c2b33bc9d5ffab0d74f0b4655088acff91b743df
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-04-09 14:40:40 +0200

    Handle null

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		TokenExtractor extractor = new DefaultTokenExtractor();
 		String token = extractor.extractToken("...");
-		System.out.println("The token has a length of " + token.length());
+		if (token == null) {
+			System.out.println("No token");
+		}
+		else {
+			System.out.println("The token has a length of " + token.length());
+		}
 	}
 }
